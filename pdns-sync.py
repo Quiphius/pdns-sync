17915e661aaf5696beddc095c74338568e1f1a0b
Mikael Olofsson <oet@oet.nu>
2014-10-23 23:04:10 +0200

    Rename ttl to cur_ttl

--- pdns-sync.py
+++ pdns-sync.py
@@ -3,24 +3,28 @@
 import fileinput
 import psycopg2
 
-ttl = 3600
+cur_ttl = 3600
+all_domains = {}
 
 class Domain:
-    def __init__(self, name, ns, email):
+    def __init__(self, name, ns, email, ttl):
         self.name = name
         self.ns = ns
         self.email = email
+        self.ttl = ttl
     def __str__(self):
-        return '%s %s %s' % (self.name, self.ns, self.email)
+        return '%s %s %s %s' % (self.name, self.ns, self.email, self.ttl)
 
 def main():
     for line in fileinput.input():
         s = line.split()
         print s
         if s[0] == 'T':
-            ttl = s[1]
+            cur_ttl = s[1]
         elif s[0] == 'D':
-            d = Domain(s[1], s[2], s[3])
+            d = Domain(s[1], s[2], s[3], cur_ttl)
             print d
+            all_domains[s[1]] = d
+    print all_domains
 
 main()
