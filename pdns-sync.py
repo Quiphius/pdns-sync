ffb5daa740d5975ee80132c3a4001ea6df137467
Mikael Olofsson <oet@oet.nu>
2014-10-25 23:19:42 +0200

    Started creating a "zone" file from imput

--- pdns-sync.py
+++ pdns-sync.py
@@ -6,6 +6,7 @@ import re
 from domain import Domain
 
 cur_ttl = 3600
+cur_domain = None
 all_domains = {}
 
 def valid_ip(a):
@@ -17,20 +18,23 @@ def find_domain(a):
     x = len(ta)
     while x > 1:
         cur = '.'.join(ta[-x:])
-        print cur
+        print(cur)
         if cur in all_domains:
-            print 'found'
             return all_domains[cur]
         x -= 1
     return None
 
 def main():
+
+    row = 0
+
     for line in fileinput.input():
+        row += 1
         line = line.rstrip('\n\r')
         if not line:
             continue
         s = line.split()
-        print s
+        print(s)
         if s[0] =='#':
             continue
         elif s[0] == 'T':
@@ -38,9 +42,20 @@ def main():
         elif s[0] == 'D':
             d = Domain(s[1], s[2], s[3], cur_ttl)
             all_domains[s[1]] = d
+            current_domain = d
+        elif s[0] == 'N':
+            for ns in s[1:]:
+                d.add_ns(ns)
+        elif s[0] == 'M':
+            d.add_mx(s[1], s[2])
+        elif s[0] == 'C':
+            find_domain(s[1]).add_cname(s[1], s[2])
+        elif valid_ip(s[0]):
+            print('Found A')
+        else:
+            print('Invalid row %d' % row);
 
-    print all_domains
 
 main()
-print find_domain('flurba.nu')
-print find_domain('foo.se')
+
+all_domains['oet.nu'].dump_domain()
