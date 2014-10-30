8679b42b8508bdae9f119c60be2fceee22caf720
Mikael Olofsson <Mikael.Olofsson@cybercom.com>
2014-10-30 14:25:14 +0100

    Rewrote arghandling

--- pdns-sync.py
+++ pdns-sync.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import fileinput
+import argparse
 from domain import Domain
 from utils import find_domain, gen_ptr, check_ipv4, check_ipv6
 from database import db_connect, db_get_domains, db_create_domains, db_delete_domains
@@ -14,88 +14,91 @@ all_db_domains = {}
 warning = 0
 error = 0
 
-def main():
-
-    row = 0
+def parse(fname):
     global warning, error
+    row = 0
 
-    for line in fileinput.input():
-        row += 1
-        line = line.rstrip('\n\r')
-        if not line:
-            continue
-        s = line.split()
-        sl = len(s)
-        if s[0] =='#':
-            continue
-        elif s[0] == 'T':
-            if sl == 2:
-                if s[1].isdigit() and int(s[1]) > 0:
-                    cur_ttl = int(s[1])
-                else:
-                    print('W: Not a valid TTL value on line %d' % row)
-                    warning += 1
-            else:
-                print('W: No arguments for TTL value on line %d' % row)
-                warning += 1
-        elif s[0] == 'D':
-            if sl == 4:
-                if s[1] not in all_domains:
-                    d = Domain(s[1], s[2], s[3], cur_ttl)
-                    all_domains[s[1]] = d
-                    current_domain = d
-                else:
-                    print('E: duplicate domain %s at line %d' %(s[1], row))
-                    error += 1
-            else:
-                print('E: Wrong number of arguments for Domain on line %d' % row)
-                error += 1
-        elif s[0] == 'N':
-            if sl > 1:
-                for ns in s[1:]:
-                    d.add_record(d.name, 'NS', ns, 0, cur_ttl)
-            else:
-                print('W: No arguments for NS on line %d' % row)
-                warning += 1
-        elif s[0] == 'M':
-            if sl > 1:
-                prio = 10
-                for x in s[1:]:
-                    if x.isdigit():
-                        prio = int(x)
-                    else:
-                        d.add_record(d.name, 'MX', x, prio, cur_ttl)
-            else:
-                print('W: No arguments for MX on line %d' % row)
-                warning += 1
-        elif s[0] == 'C':
-            if sl == 3:
-                find_domain(s[1], all_domains).add_record(s[1], 'CNAME', s[2], 0, cur_ttl)
-            else:
-                print('W: Wrong number of arguments for CNAME on line %d' % row)
-                warning += 1
-        elif check_ipv4(s[0]):
-            if sl > 1:
-                for x in s[1:]:
-                    d = find_domain(x, all_domains)
-                    if d:
-                        d.add_record(x, 'A', s[0], 0, cur_ttl)
-                        ptr = gen_ptr(s[0])
-                        ptrd = find_domain(ptr, all_domains)
-                        if ptrd:
-                            ptrd.add_record(ptr, 'PTR', x, 0, cur_ttl)
+    try:
+        with open(fname) as f:
+            for line in f.readlines():
+                row += 1
+                line = line.rstrip('\n\r')
+                if not line:
+                    continue
+                s = line.split()
+                sl = len(s)
+                if s[0] =='#':
+                    continue
+                elif s[0] == 'T':
+                    if sl == 2:
+                        if s[1].isdigit() and int(s[1]) > 0:
+                            cur_ttl = int(s[1])
                         else:
-                            print('W: Missing domain for PTR %s on line %s' % (ptr, row))
+                            print('W: Not a valid TTL value on line %d' % row)
                             warning += 1
                     else:
-                        print('W: Missing domain for A %s on line %s' % (x, row))
+                        print('W: No arguments for TTL value on line %d' % row)
                         warning += 1
-            else:
-                print('W: No names for A on line %d' % row)
-                warning += 1
-        else:
-            print('W: Invalid row %d' % row);
-            warning += 1
+                elif s[0] == 'D':
+                    if sl == 4:
+                        if s[1] not in all_domains:
+                            d = Domain(s[1], s[2], s[3], cur_ttl)
+                            all_domains[s[1]] = d
+                            current_domain = d
+                        else:
+                            print('E: duplicate domain %s at line %d' %(s[1], row))
+                            error += 1
+                    else:
+                        print('E: Wrong number of arguments for Domain on line %d' % row)
+                        error += 1
+                elif s[0] == 'N':
+                    if sl > 1:
+                        for ns in s[1:]:
+                            d.add_record(d.name, 'NS', ns, 0, cur_ttl)
+                    else:
+                        print('W: No arguments for NS on line %d' % row)
+                        warning += 1
+                elif s[0] == 'M':
+                    if sl > 1:
+                        prio = 10
+                        for x in s[1:]:
+                            if x.isdigit():
+                                prio = int(x)
+                            else:
+                                d.add_record(d.name, 'MX', x, prio, cur_ttl)
+                    else:
+                        print('W: No arguments for MX on line %d' % row)
+                        warning += 1
+                elif s[0] == 'C':
+                    if sl == 3:
+                        find_domain(s[1], all_domains).add_record(s[1], 'CNAME', s[2], 0, cur_ttl)
+                    else:
+                        print('W: Wrong number of arguments for CNAME on line %d' % row)
+                        warning += 1
+                elif check_ipv4(s[0]):
+                    if sl > 1:
+                        for x in s[1:]:
+                            d = find_domain(x, all_domains)
+                            if d:
+                                d.add_record(x, 'A', s[0], 0, cur_ttl)
+                                ptr = gen_ptr(s[0])
+                                ptrd = find_domain(ptr, all_domains)
+                                if ptrd:
+                                    ptrd.add_record(ptr, 'PTR', x, 0, cur_ttl)
+                                else:
+                                    print('W: Missing domain for PTR %s on line %s' % (ptr, row))
+                                    warning += 1
+                            else:
+                                print('W: Missing domain for A %s on line %s' % (x, row))
+                                warning += 1
+                    else:
+                        print('W: No names for A on line %d' % row)
+                        warning += 1
+                else:
+                    print('W: Invalid row %d' % row);
+                    warning += 1
+    except IOError as e:
+        print '%s: %s' % (n, e.strerror)
 
 def sync():
     db_connect()
@@ -113,11 +116,21 @@ def sync():
         d = all_domains[i]
         d.sync_domain()
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', metavar='file', nargs='+', help='the files to parse')
+    parser.add_argument("-v", "--verbosity", action="count", default=0, help="increase output verbosity")
+    args = parser.parse_args()
+
+    for fname in args.files:
+        parse(fname)
+
+    print('%d error(s) and %d warning(s)' % (error, warning))
+
+    if error == 0:
+        sync()
+    else:
+        print('Errors found, not syncing')
+
+
 main()
-
-print('%d error(s) and %d warning(s)' % (error, warning))
-
-if error == 0:
-    sync()
-else:
-    print('Errors found, not syncing')
