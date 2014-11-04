a007f7ecd7d8a7060c67b378cdb8ee0fb00def16
Mikael Olofsson <Mikael.Olofsson@cybercom.com>
2014-11-03 20:01:38 -0800

    Added more info

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name='pdnssync',
-      version='0.1',
-      description='PowerDNS sync tool',
-      classifiers=[
+    version='0.1',
+    description='PowerDNS sync tool',
+    long_description='A tool to read a /etc/hosts-like file and synconize it with the database used by PowerDNS.',
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
@@ -12,15 +13,20 @@ setup(name='pdnssync',
         'Operating System :: POSIX',
         'Topic :: Internet :: Name Service (DNS)',
         'Topic :: System :: Systems Administration',
-      ],
-      keywords='powerdns hosts sync postgresql',
-      url='http://github.com/storborg/funniest',
-      author='Mikael Olofsson',
-      author_email='mikael.olofsson@oet.nu',
-      license='MIT',
-      packages=['pdnssync'],
-      scripts=['bin/pdns-sync'],
-      install_requires=[
-          'psycopg2',
-      ],
-      zip_safe=False)
+    ],
+    keywords='powerdns hosts sync postgresql',
+    url='http://github.com/storborg/funniest',
+    author='Mikael Olofsson',
+    author_email='mikael.olofsson@oet.nu',
+    license='MIT',
+    packages=['pdnssync'],
+    entry_points={
+        'console_scripts': ['pdns-sync=pdnssync.main:main'],
+    },
+    install_requires=[
+        'psycopg2',
+    ],
+    platforms=[
+        'any',
+    ],
+    zip_safe=False)
